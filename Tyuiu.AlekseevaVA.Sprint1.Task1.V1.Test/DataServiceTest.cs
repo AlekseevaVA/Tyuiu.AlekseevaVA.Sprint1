7fe003d2c6dc892a1f30031fcf19ed5f2888fa3f
AlekseevaVA <alekseevavria@mail.ru>
2025-09-28 15:23:52 +0500

    Task1

---
+++ Tyuiu.AlekseevaVA.Sprint1.Task1.V1.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.AlekseevaVA.Sprint1.Task1.V1.Lib;
+
+
+namespace Tyuiu.AlekseevaVA.Sprint1.Task1.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double a = 3.0;
+            double x = 10.0;
+            double y = 2.0;
+            var res = ds.Calculate(a, x, y);
+            Assert.AreEqual(19.666666666666668, res);
+
+        }
+    }
+}
---
+++ Tyuiu.AlekseevaVA.Sprint1.Task1.V1/Program.cs
@@ -0,0 +1,51 @@
+﻿using Tyuiu.AlekseevaVA.Sprint1.Task1.V1.Lib;
+
+
+namespace Tyuiu.AlekseevaVA.Sprint1.Task1.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Алексеева В. А. | ИИПб-25-1 ";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнила: Алексеева В. А. | ИИПб-25-1                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя искодные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле x / 3 / y + 6 * a и печатает его на      *");
+            Console.WriteLine("* экране.                                                                 *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double a, x, y;
+
+            Console.WriteLine("Введите значение A:");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение Y:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+
+            Console.WriteLine(ds.Calculate(a, x, y));
+            Console.ReadLine();
+        }
+    }
+
+}
