86ff599219e2699cf04119e7c4100da8e388b9d0
AlekseevaVA <alekseevavria@mail.ru>
2025-09-29 20:02:06 +0500

    Task4

---
+++ Tyuiu.AlekseevaVA.Sprint1.Task4.V17.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.AlekseevaVA.Sprint1.Task4.V17.Lib;
+
+namespace Tyuiu.AlekseevaVA.Sprint1.Task4.V17.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 10;
+            double y = 1;
+            double wait = 0.447;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.AlekseevaVA.Sprint1.Task4.V17/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.AlekseevaVA.Sprint1.Task4.V17.Lib;
+
+
+namespace Tyuiu.AlekseevaVA.Sprint1.Task4.V17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Алексеева В. А. | ИИПб-25-1 ";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #17                                                             *");
+            Console.WriteLine("* Выполнила: Алексеева В. А. | ИИПб-25-1                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя искодные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле и печатает результат на экране.          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x, y;
+
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение Y:");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("1 / (x - 5y)^2 = " + ds.Calculate(x, y)); 
+           
+            Console.ReadKey();
+        }
+
+    }
+}
