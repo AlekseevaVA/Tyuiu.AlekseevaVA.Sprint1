5a30e28860b7037c1ef3268abde4a5fa82e996ff
AlekseevaVA <alekseevavria@mail.ru>
2025-10-02 10:34:07 +0500

    Task7

---
+++ Tyuiu.AlekseevaVA.Sprint1.Task7.V16.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.AlekseevaVA.Sprint1.Task7.V16.Lib;
+
+namespace Tyuiu.AlekseevaVA.Sprint1.Task7.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 2; 
+            double wait = -0.105;
+            var res = ds.Calculate(x);
+            Assert.AreEqual(wait, res);
+
+
+        }
+    }
+}
---
+++ Tyuiu.AlekseevaVA.Sprint1.Task7.V16/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.AlekseevaVA.Sprint1.Task7.V16.Lib;
+
+
+namespace Tyuiu.AlekseevaVA.Sprint1.Task7.V16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Алексеева В. А. | ИИПб-25-1 ";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #16                                                             *");
+            Console.WriteLine("* Выполнила: Алексеева В. А. | ИИПб-25-1                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу: пользователь вводит текст. Напечатать строку,       *");
+            Console.WriteLine("* составленную из последних литер всех слов.                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("                   cos(x²)");
+            Console.WriteLine("  z = sin(√(x²)) + - - - -  - sin(√(x² - 1))");
+            Console.WriteLine("                    (3x³)");
+
+
+            double x;
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(x));
+
+            Console.ReadKey();
+        }
+
+    }
+}
